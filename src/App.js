fca58ad0708ca8b8b8aa0d5d543db097b8f0b594
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-03 22:38:00 +0700

    ADD:Set up start up form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [userName, setuserName] = useState("");
+  const [userPassword, setuserPassword] = useState("");
+
+  const Login = () => {
+    console.log("Your are login in");
+  };
+
+  const Resgister = () => {
+    console.log("Resgister");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>My Login System</h2>
+      <form action="#">
+        <label for="name">user Name:</label>
+        <input
+          value={userName}
+          type="text"
+          placeholder="Enter Your Name"
+          required
+          onChange={(e) => setuserName(e.target.value)}
+        />
+        <br />
+        <label for="Email:">user Email:</label>
+        <input
+          value={userPassword}
+          type="password"
+          placeholder="Enter Your Password"
+          onChange={(e) => setuserPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit" onClick={Login}>
+          Login In
+        </button>
+        <button type="submit" onClick={Resgister}>
+          Resgister
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = ({ y }) => {
+  return <h2>{y}</h2>;
+};
+
+export default Test;
